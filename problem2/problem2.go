eba38288a047daf2745ba53f8f65f321650ca3f6
M Alvi Nurhidayah <muhammadalvin213@gmail.com>
2022-01-14 15:37:49 +0000

    upload answer problem 2 part 5

--- problem2/problem2.go
+++ problem2/problem2.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	// "reflect"
+	// "strconv"
 )
 
 func Caesar(offset int, input string) string {
 	// your code here
+
+	var output string
+	
+	// constrain offset
+	if offset > (122 - 97) {
+		offset = offset % (122 - 96)
+	}
+
+	number := 0
+
+	for i := 0; i < len(input); i++ {
+		if input[i] >= 97 && input[i] <= 122 {
+			number = int(input[i]) + offset
+			if number < 97 {
+				number = 122 - (97 - number)
+			} else if number > 122 {
+				number = 97 + (number - 123)
+			}
+			output += fmt.Sprint(string(rune(number)))
+		} else {
+			output += string(input[i])
+		}
+	}
+
+	return output
 }
 
 func main() {
