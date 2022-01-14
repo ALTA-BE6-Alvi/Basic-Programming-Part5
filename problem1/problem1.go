2a9df8b26e7f950299a01e09423bebb1a29844c4
M Alvi Nurhidayah <muhammadalvin213@gmail.com>
2022-01-14 14:44:06 +0000

    upload answer problem 1 part 5

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -4,6 +4,29 @@ import "fmt"
 
 func Compare(a, b string) string {
 	// your code here
+	var hasil string
+
+	if len(a) > len(b) {
+		i := 0
+		for i < (len(a) - len(b) + 1) {
+			if b == a[i:len(b)] {
+				hasil = b
+				i = len(a)
+			}
+			i++
+		}
+	} else {
+		i := 0
+		for i < (len(b) - len(a) + 1) {
+			if a == b[i:len(a)] {
+				hasil = a
+				i = len(b)
+			}
+			i++
+		}
+	}
+
+	return hasil
 }
 
 func main() {
